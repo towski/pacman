d7eac43620f007738b9b6603859c910dc5c6ea8a
Matthew Wastrodowski <towski@gmail.com>
2012-03-01 18:26:53 -0800

    add recent games

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @matches = Matches.all(:limit => 10, :order => 'created_at desc')
     @users = User.all.sort_by(&:matches_won_count).reverse
     @teams = Team.all.sort_by(&:matches_won_count).reverse
   end
