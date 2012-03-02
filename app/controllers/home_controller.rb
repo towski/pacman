35cf82ed1734dc0fab59a4045ecfdb8e8a2b8e29
Matthew Wastrodowski <towski@gmail.com>
2012-03-01 18:28:22 -0800

    match

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @matches = Matches.all(:limit => 10, :order => 'created_at desc')
+    @matches = Match.all(:limit => 10, :order => 'created_at desc')
     @users = User.all.sort_by(&:matches_won_count).reverse
     @teams = Team.all.sort_by(&:matches_won_count).reverse
   end
