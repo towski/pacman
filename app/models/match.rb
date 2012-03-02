63274c67336b15ed4bb46bebbb85d24a05460f70
Matthew Wastrodowski <towski@gmail.com>
2012-03-02 11:00:32 -0800

    add total matches played

--- app/models/match.rb
+++ app/models/match.rb
@@ -9,6 +9,10 @@ class Match < ActiveRecord::Base
   
   belongs_to :winning_user, :class_name => 'User'
   belongs_to :winning_team, :class_name => 'Team'
+
+  named_scope :completed, lambda {
+    {:conditions => ["winning_user_id is not null OR winning_team_id is not null"]}
+  }
   
   def done?
     rounds.count == number_of_rounds
